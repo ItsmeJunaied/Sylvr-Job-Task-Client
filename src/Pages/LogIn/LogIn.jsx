704711373295b27f75db43c5121b65bf4b1dc688
Junaied <junaied526@gmail.com>
2023-07-08 22:45:10 +0600

    react hook form imlemented

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,5 +1,17 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+    }
     return (
         <div className=" flex min-h-screen bg-gradient-to-tr from-[#86198f] to-[#d946ef]">
 
@@ -12,25 +24,30 @@ const LogIn = () => {
 
             {/* login Input Fields */}
             <div className="  w-1/2 flex justify-center items-center">
-                <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+                <form onSubmit={handleSubmit(onSubmit)} className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
                     <div className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input {...register("email", { required: true })} type="text" placeholder="email" className="input input-bordered" />
+                            {errors.email && <span className="error-message">Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("password", { required: true })} placeholder="password" className="input input-bordered" />
+                            {errors.email && <span className="error-message">Email is required</span>}
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Sign Up</button>
+                            <input className="btn btn-primary" type="submit" value="Sign Up" />
                         </div>
                     </div>
-                </div>
+                    <div>
+                        <p className="mb-10 text-center"><small>New Here? <Link className="link link-primary" to={'/signup'}>Create an Account</Link></small></p>
+                    </div>
+                </form>
             </div>
         </div>
     );
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,5 +1,17 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+      } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+    }
     return (
         <div className=" flex min-h-screen bg-gradient-to-tr from-blue-800 to-purple-700">
 
@@ -12,31 +24,37 @@ const SignUp = () => {
 
             {/* login Input Fields */}
             <div className="  w-1/2 flex justify-center items-center">
-                <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
-                    <div className="card-body">
+                <form onSubmit={handleSubmit(onSubmit)} className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+                    <div className="card-body" >
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Full Name</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" {...register("Name" , { required: true })} placeholder="name" className="input input-bordered" />
+                            {errors.name && <span className=" text-red-600">Name required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" {...register("Email" , { required: true })} placeholder="email" className="input input-bordered" />
+                            {errors.name && <span className=" text-red-600">Email required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("Password" , { required: true })} placeholder="password" className="input input-bordered" />
+                            {errors.name && <span className=" text-red-600">Password required</span>}
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Sign Up</button>
+                            <input className="btn btn-primary" type="submit" value="SignUp" />
                         </div>
                     </div>
-                </div>
+                    <div>
+                        <p className="mb-10 text-center"><small>Already Have Account? <Link className="link link-primary" to={'/login'}>LogIn</Link></small></p>
+                    </div>
+                </form>
             </div>
         </div>
     );
